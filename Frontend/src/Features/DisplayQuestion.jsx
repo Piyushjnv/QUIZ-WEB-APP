70c9af09ce6605cf3e6178d30ff558b85e33a397
Piyushjnv <flightspace2@gmail.com>
2026-05-09 00:20:58 +0530

    feat: Refactor DisplayQuestion component to handle option selection and update Q_attempt to manage question state and navigation

--- Frontend/src/Features/DisplayQuestion.jsx
+++ Frontend/src/Features/DisplayQuestion.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
-function DisplayQuestion({question}) {
+function DisplayQuestion({question , Qno, sendoptionData }) {
+  const [selectedOption, setSelectedOption] = useState(null);
+
   return (
     <div className=" w-full  flex flex-col box-border">
 
         {/*  question and option  */}
       <div className=" border-2 mb-2 ">
 
-        <div className=" border rounded m-5">{question.question}</div>
+        <div className=" border rounded m-5"> <span className=" font-bold  ">{Qno} : </span> {question.question}</div>
         <div className=" border rounded flex flex-col ">
           <div className="flex text-center flex-row  h-15 border-2 m-2">
             <input
               type="radio"
+              value={question.option1}
+              checked={selectedOption === question.option1}
+              onChange={() => {
+                setSelectedOption(question.option1);
+                sendoptionData(1);
+              }}
               id="option1"
               name="option"
               placeholder={`hii`}
@@ -19,33 +28,52 @@ function DisplayQuestion({question}) {
             />
             <label className=" " htmlFor="option1">hii {question.option1}</label>
           </div>
-          <div>
+          <div className="flex text-center flex-row  h-15 border-2 m-2">
             {" "}
             <input
+            className=" m-2 border rounded"
               type="radio"
               id="option2"
+              value={question.option2}
+               checked={selectedOption === question.option2}
+              onChange={() => {
+                setSelectedOption(question.option2);
+                sendoptionData(2);
+              }}
               name="option"
               placeholder=""
               className="  border rounded"
             />
             <label htmlFor="option2">{question.option2}</label>
           </div>
-          <div>
+          <div className="flex text-center flex-row  h-15 border-2 m-2">
             {" "}
             <input
               type="radio"
               id="option3"
               name="option"
+               checked={selectedOption === question.option3}
+              onChange={() => {
+                setSelectedOption(question.option3);
+                sendoptionData(3);
+              }}
+              value={question.option3}
               placeholder=""
               className="  border rounded"
             />
             <label htmlFor="option3">{question.option3}</label>
           </div>
-          <div>
+          <div className="flex text-center flex-row  h-15 border-2 m-2">
             <input
               type="radio"
               id="option4"
+               checked={selectedOption === question.option4}
+              onChange={() => {
+                setSelectedOption(question.option4);
+                sendoptionData(4);
+              }}
               name="option"
+              value={question.option4}
               placeholder=""
               className="  border rounded"
             />
--- Frontend/src/Features/Q_attempt.jsx
+++ Frontend/src/Features/Q_attempt.jsx
@@ -1,90 +1,110 @@
-import React from 'react'
-import API from '../API/API'
-import { useState, useEffect , useId } from 'react'
-import axios from 'axios'
-import  DisplayQuestion from './DisplayQuestion'
+import React from "react";
+import API from "../API/API";
+import { useState, useEffect, useId } from "react";
+import axios from "axios";
+import DisplayQuestion from "./DisplayQuestion";
 
- function Q_attempt() {
-  const [questions, setQuestions] = useState([])
+function Q_attempt() {
+  const [questions, setQuestions] = useState([]);
+  const [questionsno, setQuestionsno] = useState(1);
+  const [selectedoption, setSelectedOption] = useState(null);
+  const handleOptionData = (incomingData) => {
+    setSelectedOption(incomingData);
+  };
+  const next = () => {
+    console.log("i am next");
+    if (selectedoption == questions.correctOption){
+      console.log("correct");
+      
+    }else {
+      console.log("incorrect");
+    }
+  };
+  const prev = () => {
+    console.log("i am prev");
+  };
 
-
-// const fetchQuestions = async  () => {
-//       try {
-//         await axios.get('http://localhost:8000/user/qsend')
-//         .then((res) =>  {
-//           console.log(res.data.data , "i am response from qsend");
-//           // return res.data.data
-//           setQuestions( res.data.data)
-//         })
-        
-//       } catch (error) {
-//         console.error('Error fetching questions:', error)
-//       }
-//     }
-  useEffect(() => {
+  useEffect( () => {
     const fetchQuestions = async () => {
       try {
-        const response = await API.get('user/qsend')
-        setQuestions(response.data.data)
+        const response = await API.get("user/qsend")
+        setQuestions(response.data.data);
       } catch (error) {
-        console.error('Error fetching questions:', error)
+        console.error("Error fetching questions:", error);
       }
-    }
+      
+    };
+ console.log(questions, "i am questions in useeffect");
+    fetchQuestions();
+  }, [questionsno]);
 
-    fetchQuestions()
-  }, [])
-console.log(questions);
+  // console.log(questions);
 
   return (
-    <div className=' flex flex-col w-full relative '>
+    <div className=" flex flex-col w-full relative ">
       {/* header */}
       <div>
-
+        <h1 className=" text-3xl font-bold text-center mt-5">
+          Attempt Questions
+        </h1>
+        <div className="text-2xl font-bold text-center border-2  bg-amber-300 ">
+          {questions.category}
+        </div>
       </div>
       {/* question section  */}
-      <div className='mt-5'>
-      <DisplayQuestion question={questions}/>
+      <div className="mt-5">
+        <DisplayQuestion  question={questions} sendoptionData={handleOptionData} Qno={questionsno} />
       </div>
-    {/* buttons at bottom for action  */}
-      <div className="flex w-full relative m-10">
-        <div className=" absolute left-0 border-2 h-8 w-15 text-center font-bold bg-[green] text-white border-[black] rounded ">
+
+      <div className="flex justify-center mt-5">
+        <div className="border-2 h-8 px-6 text-center font-bold bg-[green] text-white border-[black] rounded cursor-pointer" onClick={next}>
+          SUBMIT
+        </div>
+      </div>
+      {/* buttons at bottom for action  */}
+      <div className="flex relative m-10">
+        <div 
+        onClick={prev}
+        className=" absolute left-0 border-2 h-8 w-15 text-center font-bold bg-[green] text-white border-[black] rounded ">
           PREV
         </div>
         <div></div>
-        <div className=" absolute  right-0 border-2 h-8 w-15 text-center font-bold bg-[green]  text-white border-[black] rounded ">
+        <div
+        onClick={next}
+        className=" absolute  right-0 border-2 h-8 w-15 text-center font-bold bg-[green]  text-white border-[black] rounded ">
           NEXT
         </div>
       </div>
+      
     </div>
-  )
+  );
 }
 
-export default Q_attempt
-// {username: '69ea5ce6b7e89e3b595a60dc', question: 'jfhsdjfd', option1: 'fndsjfjdf', option2: 'mfsdnfjdf', option3: 'kfjdjhf', …}
+export default Q_attempt;
 // category
-// : 
+// :
 // "Current"
 // correctOption
-// : 
+// :
 // "3"
 // option1
-// : 
+// :
 // "fndsjfjdf"
 // option2
-// : 
+// :
 // "mfsdnfjdf"
 // option3
-// : 
+// :
 // "kfjdjhf"
 // option4
-// : 
+// :
 // "dfjfdhf"
 // question
-// : 
+// :
 // "jfhsdjfd"
 // username
-// : 
+// :
 // "69ea5ce6b7e89e3b595a60dc"
 // [[Prototype]]
-// : 
+// :
 // Object
